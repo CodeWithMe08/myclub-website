94fefffee38934d424b03df188c39cfbcc488512
Raunak Varma <raunakvarma05@gmail.com>
2023-01-03 10:10:36 +0530

    modified django admin area

--- myclub_website/events/models.py
+++ myclub_website/events/models.py
@@ -1,13 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Venue(models.Model):
     name = models.CharField('Venue Name', max_length=120)
     address = models.CharField(max_length=300)
     zip_code  = models.CharField('Zip Code', max_length=30)
     phone = models.CharField('Contact Phone', max_length=15)
-    web  = models.URLField('Website Address')
-    email_address  = models.EmailField('Email Address')
+    web  = models.URLField('Website Address', blank=True)
+    email_address  = models.EmailField('Email Address', blank=True)
 
 
 class MyClubUser(models.Model):
@@ -23,7 +23,8 @@ class Event(models.Model):
     name = models.CharField('Event Name', max_length=120)
     event_date =models.DateTimeField('Event Date')
     venue = models.ForeignKey(Venue, blank=True, null=True, on_delete=models.CASCADE)
-    manager =models.CharField(max_length=60)
+    # manager =models.CharField(max_length=60)
+    manager = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL)
     description = models.TextField(blank=True)
     attendees  = models.ManyToManyField(MyClubUser, blank = True)
     def __str__(self): 
