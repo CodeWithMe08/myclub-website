80c723392d3a9f802995f4de06c8411dfaf68913
Raunak Varma <raunakvarma05@gmail.com>
2023-01-09 13:02:50 +0530

    added logout on web page

--- myclub_website/members/views.py
+++ myclub_website/members/views.py
@@ -14,6 +14,11 @@ def login_user(request):
 			messages.success(request, ("There Was An Error Logging In, Try Again..."))	
 			return redirect('login')	
 
-
 	else:
 		return render(request, 'authenticate/login.html', {})
+
+
+def logout_user(request):
+	logout(request)
+	messages.success(request, ("You Were Logged Out!"))
+	return redirect('home')
