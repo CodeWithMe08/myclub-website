6b19091ca5b42a926177f62cc99da361abd32506
Raunak Varma <raunakvarma05@gmail.com>
2023-01-09 15:10:43 +0530

    added some more fields to django register form and styles and also locked app

--- myclub_website/members/views.py
+++ myclub_website/members/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
 from django.contrib.auth.forms import UserCreationForm
+from .forms import RegisterUserForm
 
 
 def login_user(request):
@@ -26,19 +27,20 @@ def logout_user(request):
 	return redirect('home')
 
 def register_user(request):
-	if request.method == "POST":
-		form = UserCreationForm(request.POST)
-		if form.is_valid():
-			form.save()
-			username = form.cleaned_data['username']
-			password = form.cleaned_data['password1']
-			user = authenticate(username=username, password=password)
-			login(request, user)
-			messages.success(request, ("Registration Successful!"))
-			return redirect('home')
-	else:
-		form = UserCreationForm()
-
-	return render(request, 'authenticate/register_user.html', {
-		'form':form,
-		})
+    if request.method == "POST":
+        # form = UserCreationForm(request.POST)
+        form = RegisterUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, ("Registration Successful!"))
+            return redirect('home')
+    else:
+        # form = UserCreationForm()
+        form = RegisterUserForm()
+    return render(request, 'authenticate/register_user.html', {
+        'form':form,
+        })
